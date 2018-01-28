7fcb3042fa9f4ddb9d33a51d29ad91765f9d5ab1
ErlandD <erlanddevona@gmail.com>
2018-01-28 14:12:58 +0700

    Add files via upload

---
+++ checkNumeric/isNumeric.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkNumeric
+{
+    public class isNumeric
+    {
+        public static Boolean check(String text)
+        {
+            int parsedValue;
+            if (!int.TryParse(text, out parsedValue))
+            {
+                return false;
+            } return true;
+        }
+    }
+}
